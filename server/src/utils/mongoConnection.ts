d2dddc22d083f717c0102a85276038ee6cb77ff5
kess <kesslermentele@gmail.com>
2026-03-19 20:13:37 -0700

    Encode MongoDB credentials and update connection options to resolve SSL and CA file handling

--- server/src/utils/mongoConnection.ts
+++ server/src/utils/mongoConnection.ts
@@ -1,6 +1,7 @@
 import { getSecret } from "../services/secretService";
 import {DocDBSecret} from "../types";
 import mongoose from "mongoose";
+import path from "path";
 
 let initialized = false;
 
@@ -14,11 +15,16 @@ export async function initMongo(): Promise<void> {
 
   const config = await loadMongoConfig();
 
-  const uri = `mongodb://${config.username}:${config.password}` +
-    `@${config.host}:${config.port}/?ssl=${config.ssl ? "true" : "false"}` +
-    `requestsdb?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`;
+  const username = encodeURIComponent(config.username);
+  const password = encodeURIComponent(config.password);
 
-  await mongoose.connect(uri);
+  const uri = `mongodb://${username}:${password}@${config.host}:${config.port}` +
+    `/requestsdb?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`;
+
+  await mongoose.connect(uri, {
+    tls: config.ssl,
+    tlsCAFile: path.resolve(__dirname, "../../global-bundle.pem"),
+  });
 
   initialized = true;
 }
